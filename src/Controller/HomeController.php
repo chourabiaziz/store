bfef5ce9de95a363007f8e92177898c4f60f5713
chourabiaziz <chourabiaziz007@gmail.com>
2024-11-10 18:05:27 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('', name: 'app_home')]
+    public function index(): Response
+    {
+
+        if ($this->isGranted("ROLE_ADMIN")) {
+            return $this->render('admin/home.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);     
+           }
+       
+            
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
